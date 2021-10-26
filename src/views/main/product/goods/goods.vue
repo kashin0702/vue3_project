13446c948bb55c167f77173db5911e2ab09cf493
guyufeng <32313941@qq.com>
2021-10-26 08:47:58 +0800

    10.26

--- src/views/main/product/goods/goods.vue
+++ src/views/main/product/goods/goods.vue
@@ -1,17 +1,23 @@
 <template>
-  <div>
-
+  <div class="goods">
+    <page-content :tableContentConfig="tableContentConfig"></page-content>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-
+import { PageContent } from '@/components/page-content'
+import { tableContentConfig } from './config/content.config'
 export default defineComponent({
+  components: {
+    PageContent
+  },
   setup () {
     
 
-    return {}
+    return {
+      tableContentConfig
+    }
   }
 })
 </script>
